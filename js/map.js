4bd728ba49328b3ebbd058712ae3ae335fc52436
Abdulrhman19 <am_alharthi19@hotmail.com>
2021-07-14 04:16:22 +0300

    Final Version

---
+++ js/map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWJkdWxyaG1hbjE5IiwiYSI6ImNrcjJxYnZuNTJibnkzMXFoaDkzN3lrc2QifQ.Ek_DsPzQV_-2jLFVTBQlJg";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+//   center: [21.437273, 40.512714],
+  zoom: 18,
+});
